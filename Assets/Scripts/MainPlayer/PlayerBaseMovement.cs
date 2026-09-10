e68c9e12e269e79b72fae6dd42f194f995f7a9bd
R4don1T19 <311372871+R4don1T19@users.noreply.github.com>
2026-09-10 16:56:43 +0300

    Player now can Rotates with all these Trigger-zones

--- Assets/Scripts/MainPlayer/PlayerBaseMovement.cs
+++ Assets/Scripts/MainPlayer/PlayerBaseMovement.cs
@@ -1,10 +1,12 @@
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerBaseMovement : MonoBehaviour
 {
-    public static PlayerMovement Instance;
+    public static PlayerBaseMovement Instance;
     private PlayerSurfaceConfig PSC;
     private Rigidbody2D rb;
+    private Quaternion FixedA = new Quaternion(0, 180, 0, 0);
+    private Quaternion FixedB = new Quaternion(0, 0, 0, 0);
     private int fixedY = 0;
     public int speed;
     private void Awake()
@@ -26,13 +28,22 @@ public class PlayerMovement : MonoBehaviour
     }
     private void FixedUpdate()
     {
-        Walk(Input.GetAxisRaw("Horizontal"));
+        float localDirection = Input.GetAxisRaw("Horizontal");
+        Walk(localDirection);
+        FlipThePlayer(localDirection);
     }
-    private void Walk(float localDirection)
+    private void Walk(float direction)
     {
-        Vector2 Walk = new Vector2(localDirection * speed, fixedY);
+        Vector2 Walk = new Vector2(direction * speed, fixedY);
         // Игрик зафиксирован, чтобы не было воздействия на этот вектор движения.
         // За место Y в PSD установлен якорь(surfaceAnchor), который и тянет вниз.
         rb.Slide(Walk, Time.fixedDeltaTime, PSC.SlideConfig);
+    }
+    private void FlipThePlayer(float direction)
+    {
+        if (direction < 0)
+            transform.rotation = FixedA;
+        else if (direction > 0)
+            transform.rotation = FixedB;
     }
 }
